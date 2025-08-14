3826831d7f221b38e9d1ca78f96a2986103616b9
Micaela Juarez <micaelajuarez095@gmail.com>
2025-08-14 18:52:26 -0300

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function añadirAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    let nombreLimpio = nombre.trim();
+
+    if (nombreLimpio !== "") {
+        amigos.push(nombreLimpio); // Guardar en el array
+        document.getElementById("amigo").value = ""; // Limpiar el input
+        mostrarLista(); // Redibujar la lista completa
+    } else {
+        alert("Por favor, inserte un nombre");
+    }
+}
+
+function mostrarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Vaciar la lista
+
+    amigos.forEach(nombre => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
